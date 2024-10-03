4f6ac20f179f7d4ca15b10c168062ad35dbe2a77
Jow12560 <jowrock555@gmail.com>
2024-10-03 15:07:11 +0700

    no cors

--- abac-front/src/config/axios.config.ts
+++ abac-front/src/config/axios.config.ts
@@ -2,10 +2,7 @@ import axios from "axios";
 
 const ENDPOINT = import.meta.env.VITE_BACKEND_URL;
 
-const authAxiosClient = axios.create({
-  baseURL: ENDPOINT,
-  withCredentials: true, // If you're dealing with cookies or sessions
-});
+const authAxiosClient = axios.create();
 
 authAxiosClient.interceptors.request.use((config) => {
   config.headers["x-api-key"] = import.meta.env.VITE_API_KEY;
@@ -18,10 +15,7 @@ authAxiosClient.interceptors.response.use(
     return response;
   },
   (error) => {
-    if (!error.response) {
-      throw new Error("Network error. Please try again later.");
-    }
-    throw new Error(error.response.data.message || "An error occurred.");
+    throw new Error(error.response.data.message);
   }
 );
 
